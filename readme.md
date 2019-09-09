3c69354cd9ff950c58759c426b087f3d5a720aa9
QuincyX <likequincy@outlook.com>
2019-09-09 11:50:12 +0800

    u

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # less-toolkit
+> Quincy's less 工具包
 
 ## theme
 
--- test/t.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-const r = path.resolve(__dirname)
-let a = '../../../src/mixin/index.less'
-console.log(path.posix.normalize(r))
